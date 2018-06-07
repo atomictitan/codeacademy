2caf075c49c7d42606c4f941c0c15d846972f1fc
Ben Fisher <ben.fisher@caveon.com>
2018-06-07 10:01:50 -0600

    fix while loop bug

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -2,9 +2,9 @@
 
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     let board = [];
-    for (let i = 0; i < numberOfRows.length; i++) {
+    for (let i = 0; i < numberOfRows; i++) {
         let row = [];
-        for (let j = 0; j < numberOfColumns[i].length; j++) {
+        for (let j = 0; j < numberOfColumns; j++) {
             row.push(' ');
         }
         board.push(row);
@@ -14,35 +14,33 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 
  const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     let board = [];
-    for (let i = 0; i < numberOfRows.length; i++) {
+    for (let i = 0; i < numberOfRows; i++) {
         let row = [];
-        for (let j = 0; j < numberOfColumns[i].length; j++) {
+        for (let j = 0; j < numberOfColumns; j++) {
             row.push(null);
         }
         board.push(row);
     }
-
     let numberOfBombsPlaced = 0;
-
     while (numberOfBombsPlaced < numberOfBombs) {
         let randomRowIndex = Math.floor(Math.random() * numberOfRows);
         let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
         board[randomRowIndex][randomColumnIndex] = 'B';
-        numberOfBombs++;
+        numberOfBombsPlaced++;
     }
     return board;
  };
-
-
 
 const printBoard = board => {
     console.log(board.map(row => row.join(' | ')).join('\n'));
 }
 
 let playerBoard = generatePlayerBoard(3, 4);
+console.log('player board made');
 let bombBoard = generateBombBoard(3, 4, 5);
 
 console.log('Player Board:');
 printBoard(playerBoard);
+
 console.log('Bomb Board:');
 printBoard(bombBoard);
